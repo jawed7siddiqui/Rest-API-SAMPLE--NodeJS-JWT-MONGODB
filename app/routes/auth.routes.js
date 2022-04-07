2bd1a78ea70bbacec649a3a200a68677fa4e86b9
jawed7siddiqui@gmail.com <jawed7siddiqui@gmail.com>
2022-04-07 19:49:15 +0530

    cors

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -5,11 +5,15 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, Origin, Content-Type, Accept,X-Auth-Token'"
     );
     res.header(
-      "Access-Control-Allow-Origin", "http://54.218.130.89:3000"
+      "Access-Control-Allow-Origin", "*"
       ); 
+      res.header(
+        "Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+        ); 
+      
     next();
   });
 
